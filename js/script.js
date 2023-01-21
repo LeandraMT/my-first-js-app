7bb15d43bce21e6444e490a6c294130eb6e5b1ad
LeandraMT <magantierleandra@gmail.com>
2023-01-21 11:33:33 +0100

    Update script.js
    This is the Pokemon list for the project

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-alert('Hello World!');
-
-let favouriteFood = 'Spaghetti';
-    document.write(favouriteFood);
+// This will be the three Pokemons used
+let pokemonList = [
+    {name: 'Charmander', height: 0.6, type: 'fire'},
+    {name: 'Squirtle', height: 0.5, type: ['grass', 'water']},
+    {name: 'Pikachu', height: 0.4, type: 'electric'}
+];
