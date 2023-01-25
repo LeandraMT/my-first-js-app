9ca0f4cdf519600b6c45bacf49135a02d57515bf
LeandraMT <magantierleandra@gmail.com>
2023-01-25 09:59:54 +0100

    Update script.js
    Added more pokemons.
    Added the Loop 'for'

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-// This will be the three Pokemons used
+// This will be the five Pokemons used
 let pokemonList = [
     {name: 'Charmander', height: 0.6, type: 'fire'},
-    {name: 'Squirtle', height: 0.5, type: ['grass', 'water']},
+    {name: 'Bulbasaur', height:0.6, type: ['grass', 'water']},
+    {name: 'Vulpix', height: 0.6, type: 'rock'},
+    {name: 'Squirtle', height: 1.5, type: ['grass', 'water']},
     {name: 'Pikachu', height: 0.4, type: 'electric'}
 ];
+
+//Loop 'for' is used
+for (let index = 0; index < pokemonList.length; index++){
+    if (pokemonList[i].height >= 1.5){
+        document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + 'Wow, that is a big pokemon!' + '<br>');
+    } else if (pokemonList[i].height == 0.6){
+        document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + 'That is a medium Pokemon' + '<br>');
+    } else {
+        document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + 'That is a small Pokemon' + '<br>');
+    }
+}
