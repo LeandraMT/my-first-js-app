e8ed8d5fb0c0da0d76814432a789af15773dd005
LeandraMT <magantierleandra@gmail.com>
2023-01-25 10:15:19 +0100

    Update script.js
    Removed code

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ let pokemonList = [
     {name: 'Vulpix', height: 0.6, type: 'rock'},
     {name: 'Squirtle', height: 1.5, type: ['grass', 'water']},
     {name: 'Pikachu', height: 0.4, type: 'electric'}
-];
+]
 
 //Loop 'for' is used
 for (let index = 0; index < pokemonList.length; index++){
     if (pokemonList[i].height >= 1.5){
-        document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + 'Wow, that is a big pokemon!' + '<br>');
+        console.log(pokemonList[i].name + ' Wow, that is a big pokemon!' + '<br>');
     } else if (pokemonList[i].height == 0.6){
-        document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + 'That is a medium Pokemon' + '<br>');
+        console.log(pokemonList[i].name + ' That is a medium Pokemon' + '<br>');
     } else {
-        document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + 'That is a small Pokemon' + '<br>');
+        console.log(pokemonList[i].name + ' That is a small Pokemon' + '<br>');
     }
 }
