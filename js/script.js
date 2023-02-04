81b61a05511647a61eaca6bb8b6be58c206ece4b
LeandraMT <magantierleandra@gmail.com>
2023-02-04 14:11:36 +0100

    updating forEach loop

--- js/script.js
+++ js/script.js
@@ -32,27 +32,16 @@ let pokemonRepository = (function(){
     function add(pokemon){
         pokemonList.push(pokemon);
     }
-
     function getAll(){
         return pokemonList;
     }
-
     return {
         add: add,
         getAll: getAll
-    };
+    }
 })();
 
-// forEach Loop
-pokemonList.forEach(function(pokemon){
-    document.write(pokemon.name + ' (height:' + pokemon.height + ')');
-});
-
-pokemonRepository.getAll().forEach(function(pokemon){
-    document.write(pokemon.name + pokemon.height);
-});
-
-/* before forEach Loop
+/* before the forEach Loop
 for (let i = 0; i < pokemonList.length; i++){
     if (pokemonList[i].height >= 1.5){
         document.write('<p class="squirtle">' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + ' - Wow that is a big Pokémon!' + '</p>')
@@ -63,3 +52,18 @@ for (let i = 0; i < pokemonList.length; i++){
     }
 }
 */
+
+// forEach loop
+pokemonList.forEach(function(pokemon){
+    if (pokemonList.height >= 1.5){
+        document.write('<p class="squirtle">' + pokemonList.name + ' (height:' + pokemonList.height + ')' + ' - Wow that is a big Pokémon!' + '</p>')
+    }else if (pokemonList.height === 0.6){
+        document.write('<p>' + pokemonList.name + ' (height:' + pokemonList.height + ')')// + ' - That is a medium Pokemon' + '</p>')
+    }else {
+        document.write('<p>' + pokemonList.name + ' (height:' + pokemonList.height + ')')// + ' - That is a small Pokemon' + '</p>')
+    }
+});
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    document.write(pokemon.name + pokemon.height);
+});
