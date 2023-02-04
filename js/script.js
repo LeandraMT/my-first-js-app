501c54f3a2f6d2dc0c4a908f9e62751db183c240
LeandraMT <magantierleandra@gmail.com>
2023-02-04 13:03:11 +0100

    replacing for loop with forEach loop

--- js/script.js
+++ js/script.js
@@ -1,33 +1,58 @@
 // This will be the five Pokemons used
-let pokemonList = [
-    {
-        name: 'Charmander', 
-        height: 0.6, 
-        type: 'fire'
-    },
-    {
-        name: 'Bulbasaur', 
-        height:0.6, 
-        type: ['grass', 'water']
-    },
-    {
-        name: 'Vulpix', 
-        height: 0.6, 
-        type: 'rock'
-    },
-    {
-        name: 'Squirtle', 
-        height: 1.5, 
-        type: ['grass', 'water']
-    },
-    {
-        name: 'Pikachu', 
-        height: 0.4, 
-        type: 'electric'
-    }
-];
+// IIFE
+let pokemonRepository = (function(){
+    let pokemonList = [
+        {
+            name: 'Charmander', 
+            height: 0.6, 
+            type: 'fire'
+        },
+        {
+            name: 'Bulbasaur', 
+            height:0.6, 
+            type: ['grass', 'water']
+        },
+        {
+            name: 'Vulpix', 
+            height: 0.6, 
+            type: 'rock'
+        },
+        {
+            name: 'Squirtle', 
+            height: 1.5, 
+            type: ['grass', 'water']
+        },
+        {
+            name: 'Pikachu', 
+            height: 0.4, 
+            type: 'electric'
+        }
+    ]
 
-//Loop 'for' with conditional statement. Leaving only the height of each pokemon and stating only 1 that is big.
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    function getAll(){
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+// forEach Loop
+pokemonList.forEach(function(pokemon){
+    document.write(pokemon.name + ' (height:' + pokemon.height + ')');
+});
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    document.write(pokemon.name + pokemon.height);
+});
+
+/* before forEach Loop
 for (let i = 0; i < pokemonList.length; i++){
     if (pokemonList[i].height >= 1.5){
         document.write('<p class="squirtle">' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + ' - Wow that is a big Pokémon!' + '</p>')
@@ -37,3 +62,4 @@ for (let i = 0; i < pokemonList.length; i++){
         document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')')// + ' - That is a small Pokemon' + '</p>')
     }
 }
+*/
